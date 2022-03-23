c3ab90cc5d0109d909d56ffb268d79114ed91a6b
Varesa1 <necaticoskuncopur@gmail.com>
2022-03-23 16:08:51 +0300

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { FaPlusSquare } from 'react-icons/fa'
+
+function Form({setInputText, todos, setTodos, inputText, setStatus}) {
+
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value);
+  }
+
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([...todos, {text: inputText, completed: false, id: Math.floor(Math.random() * 10000) }]);
+    setInputText('')
+  }
+
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
+  }
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          className='todo-input'
+          onChange={inputTextHandler}
+          value={inputText}
+        />
+        <button
+          type='submit'
+          className='todo-button'
+          onClick={submitTodoHandler}
+        >
+          <FaPlusSquare />
+        </button>
+        <div className="select">
+          <select onChange={statusHandler} name="todos" className='filter-todo'>
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="uncompleted">Uncompleted</option>
+          </select>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Form
